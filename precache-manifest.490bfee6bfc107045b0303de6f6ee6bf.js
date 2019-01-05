82efe5794939e8381b886b4d313314e5e2c25c5d
adnenre <adnen.rebai@gmail.com>
2019-01-05 09:31:55 +0100

    Updates

---
+++ precache-manifest.490bfee6bfc107045b0303de6f6ee6bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c1ff60ee3ccc6c11f7d",
+    "url": "/messaging-app/static/js/runtime~main.7c1ff60e.js"
+  },
+  {
+    "revision": "08ae8fde51925749b3b0",
+    "url": "/messaging-app/static/js/main.08ae8fde.chunk.js"
+  },
+  {
+    "revision": "3e6ddc51ea818a31fe57",
+    "url": "/messaging-app/static/js/1.3e6ddc51.chunk.js"
+  },
+  {
+    "revision": "08ae8fde51925749b3b0",
+    "url": "/messaging-app/static/css/main.add26153.chunk.css"
+  },
+  {
+    "revision": "4d5a22cecb070bc374d2df0bc106812b",
+    "url": "/messaging-app/index.html"
+  }
+];
