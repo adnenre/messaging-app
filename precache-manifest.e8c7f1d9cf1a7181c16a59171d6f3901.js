4ae2d52174acbdcc438456128f8e932f6f0e53a4
adnen rebai <adnen.Rebai@gmail.com>
2022-03-22 00:34:04 +0100

    Updates

---
+++ precache-manifest.e8c7f1d9cf1a7181c16a59171d6f3901.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e5c9acf07d43a60b2e",
+    "url": "/messaging-app/static/css/main.4171a14d.chunk.css"
+  },
+  {
+    "revision": "82e5c9acf07d43a60b2e",
+    "url": "/messaging-app/static/js/main.82e5c9ac.chunk.js"
+  },
+  {
+    "revision": "40fbf486c0a6182d768b",
+    "url": "/messaging-app/static/js/1.40fbf486.chunk.js"
+  },
+  {
+    "revision": "7c1ff60ee3ccc6c11f7d",
+    "url": "/messaging-app/static/js/runtime~main.7c1ff60e.js"
+  },
+  {
+    "revision": "9ddfe4e17eabd953247feaf4f1f6a48f",
+    "url": "/messaging-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ac4afa6dfecc3612fdf52806cc1050ed.js"
+  "/messaging-app/precache-manifest.e8c7f1d9cf1a7181c16a59171d6f3901.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/messaging-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
