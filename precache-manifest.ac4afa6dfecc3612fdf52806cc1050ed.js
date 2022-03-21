1ce9238b7f83649800845eb157e26d150dac116b
adnen rebai <adnen.Rebai@gmail.com>
2022-03-22 00:31:45 +0100

    Updates

---
+++ precache-manifest.ac4afa6dfecc3612fdf52806cc1050ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4a39959f70789d7932",
+    "url": "./static/css/main.4171a14d.chunk.css"
+  },
+  {
+    "revision": "9d4a39959f70789d7932",
+    "url": "./static/js/main.9d4a3995.chunk.js"
+  },
+  {
+    "revision": "40fbf486c0a6182d768b",
+    "url": "./static/js/1.40fbf486.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4d49367a237803fa9259e3103bb8c29f",
+    "url": "./index.html"
+  }
+];
